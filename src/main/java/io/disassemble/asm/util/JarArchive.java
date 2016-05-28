92faf9a44b66f412a24c5bc1b5502a74d1e89f58
Christopher Carpenter <ccarpenter04@swmich.edu>
2016-05-28 02:47:06 -0400

    Begin work on making JarArchive Concurrent/Parallel friendly. Also better built state management.

--- src/main/java/io/disassemble/asm/util/JarArchive.java
+++ src/main/java/io/disassemble/asm/util/JarArchive.java
@@ -12,9 +12,11 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
-import java.util.Enumeration;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
@@ -24,14 +26,27 @@ import java.util.jar.JarOutputStream;
  * @since 2/12/16
  */
 public class JarArchive {
-
-    private final Map<String, ClassFactory> nodes = new HashMap<>();
-    private final Map<String, byte[]> resources = new HashMap<>();
-
+    private final ConcurrentHashMap<String, ClassFactory> classes = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, byte[]> resources = new ConcurrentHashMap<>();
     private final File file;
+    private boolean built = false;
 
     public JarArchive(File file) {
         this.file = file;
+    }
+
+    public ConcurrentMap<String, ClassFactory> classes() {
+        if (!built) {
+            throw new IllegalStateException("The JarArchive must be built before its classes can be retrieved.");
+        }
+        return classes;
+    }
+
+    public ConcurrentMap<String, byte[]> resources() {
+        if (!built) {
+            throw new IllegalStateException("The JarArchive must be built before its resources can be retrieved.");
+        }
+        return resources;
     }
 
     /**
@@ -40,7 +55,7 @@ public class JarArchive {
      * @param cfv The visitor to dispatch.
      */
     public void dispatch(ClassFactoryVisitor cfv) {
-        for (ClassFactory factory : nodes.values()) {
+        for (ClassFactory factory : classes.values()) {
             factory.accept(cfv);
         }
     }
@@ -51,7 +66,7 @@ public class JarArchive {
      * @param cmv The visitor to dispatch.
      */
     public void dispatch(ClassMethodVisitor cmv) {
-        for (ClassFactory factory : nodes.values()) {
+        for (ClassFactory factory : classes.values()) {
             factory.dispatch(cmv);
             if (cmv.locked()) {
                 return;
@@ -59,25 +74,52 @@ public class JarArchive {
         }
     }
 
-    private byte[] inputToBytes(InputStream in) {
+    /**
+     * Completely reads an open input stream and then closes it.
+     *
+     * @param in The InputStream to be read
+     * @return a non-null {@code byte[]} containing the results the now closed <@code InputStream>
+     */
+    private byte[] readInputStream(InputStream in) throws IOException {
         try (ReadableByteChannel inChannel = Channels.newChannel(in)) {
-            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-                try (WritableByteChannel outChannel = Channels.newChannel(baos)) {
-                    ByteBuffer buffer = ByteBuffer.allocate(4096);
-                    while (inChannel.read(buffer) != -1) {
-                        buffer.flip();
-                        outChannel.write(buffer);
-                        buffer.compact();
-                    }
+            //ByteArrayOutputStreams don't need to be closed.
+            /*
+             TODO
+             The default initial capacity of a ByteArrayOutputStream is only 32.
+             We need to set an initial capacity becuause when it's exceeded it only doubles.
+             That means it'll resize a LOT on large classes.
+             I recommend we check if we have an InputStream with a reliable #available method.
+             If so, we should use an initial capacity based on that.
+             Otherwise, I recommend something along the lines of Math.min(4096, Math.max(32, in.available()));
+             */
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            /*
+            TODO
+            Is writing to a WriteByteChannel that wraps a ByteArrayOutputStream more efficient?
+            I don't see how it could be. It seems like it would just add overhead. I understand that
+            using NIO for reading would yield better results.
+            */
+            try (WritableByteChannel outChannel = Channels.newChannel(baos)) {
+                /*
+                TODO
+                Benchmark if 4096 is optimal, allocating 8192 my be more efficient because that's
+                the default transfer size of ReadableByteChannelImpl. However, if {@code in} is
+                an instance of FileInputStream the default transfer size may be different
+                because Channels.newChannel(InputStream) will instead return an implementation of
+                FileChannelImpl.
+                */
+                ByteBuffer buffer = ByteBuffer.allocate(4096);
+                while (inChannel.read(buffer) != -1) {
                     buffer.flip();
-                    while (buffer.hasRemaining()) {
-                        outChannel.write(buffer);
-                    }
-                    return baos.toByteArray();
+                    outChannel.write(buffer);
+                    buffer.compact();
                 }
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    outChannel.write(buffer);
+                }
+                return baos.toByteArray();
             }
-        } catch (IOException e) {
-            return new byte[0];
         }
     }
 
@@ -91,50 +133,72 @@ public class JarArchive {
     }
 
     /**
-     * Builds the class map.
+     * Gets the build state, that is whether or not this JarArchive has been built.
      *
-     * @return A map of classes built.
+     * @return true if this JarArchive has been built.
      */
-    public Map<String, ClassFactory> build() {
-        if (!nodes.isEmpty()) {
-            return nodes;
+    public boolean built() {
+        return built;
+    }
+
+    /**
+     * Builds the class and resource maps.
+     *
+     * @return The amount of milliseconds it took to build.
+     */
+    public long build() throws IOException {
+        return build(true);
+    }
+
+    /**
+     * Builds the class and resource maps.
+     *
+     * @param parallel a boolean that decides if the classes and resources should be built in parallel
+     * @return The amount of milliseconds it took to build.
+     */
+    public long build(boolean parallel) throws IOException {
+        long time = System.currentTimeMillis();
+        if (built()) {
+            throw new IllegalStateException("The JarArchive cannot be built more than once.");
         }
-        try {
-            JarFile jar = new JarFile(file);
-            Enumeration<JarEntry> entries = jar.entries();
-            while (entries.hasMoreElements()) {
-                JarEntry entry = entries.nextElement();
-                String name = entry.getName();
+        try (JarFile jar = new JarFile(file)) {
+            ArrayList<JarEntry> entries = Collections.list(jar.entries());
+            //TODO size
+            ConcurrentHashMap<String, InputStream> entryStreams = new ConcurrentHashMap<>();
+            for (JarEntry entry : entries) {
+                entryStreams.put(entry.getName(), jar.getInputStream(entry));
+            }
+            //TODO verify this is proper parallelism threshold
+            //https://github.com/TSedlar/Sedlar-Bytecode/commit/15c256e0e4256da2a961689092fd9ec37528ec39#diff-a6f2dfa4ed5757affb73b1feb6832181R95
+            entryStreams.forEach(parallel ? 1 : Long.MAX_VALUE, (name, input) -> {
+                //TODO figure out what to do about the exception handling...
                 if (name.endsWith(".class")) {
                     ClassNode cn = new ClassNode();
-                    try (InputStream input = jar.getInputStream(entry)) {
-                        ClassReader reader = new ClassReader(inputToBytes(input));
-                        reader.accept(cn, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
-                        nodes.put(name.replace(".class", ""), new ClassFactory(cn));
-                    }
+                    ClassReader reader = new ClassReader(readInputStream(input));
+                    reader.accept(cn, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
+                    classes.put(name.replace(".class", ""), new ClassFactory(cn));
                 } else {
-                    if (!name.equals("META-INF/MANIFEST.MF")) {
-                        try (InputStream input = jar.getInputStream(entry)) {
-                            resources.put(name, inputToBytes(input));
-                        }
-                    }
+                    resources.put(name, readInputStream(input));
                 }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Error building classes (" + file.getName() + "): ", e.getCause());
+                input.close();
+            });
         }
-        return nodes;
+        built = true;
+        return System.currentTimeMillis() - time;
     }
 
     /**
      * Writes the classes to the given file.
      *
      * @param target The file to write to.
-     * @param args The ClassWriter args to use.
+     * @param args   The ClassWriter args to use.
      */
     public void write(File target, int args) {
+        if(!built()){
+            throw new IllegalStateException("You cannot write a JarArchive until it has been built.");
+        }
         try (JarOutputStream output = new JarOutputStream(new FileOutputStream(target))) {
-            for (Map.Entry<String, ClassFactory> entry : build().entrySet()) {
+            for (Map.Entry<String, ClassFactory> entry : classes.entrySet()) {
                 ClassFactory factory = entry.getValue();
                 output.putNextEntry(new JarEntry(factory.name().replaceAll("\\.", "/") + ".class"));
                 ClassWriter writer = new ClassWriter(args);
@@ -155,25 +219,25 @@ public class JarArchive {
 
     /**
      * Writes the classes back out to itself.
-     * 
+     *
      * @param args The ClassWriter arguments to use.
      */
     public void write(int args) {
         write(file, args);
     }
-    
+
     /**
      * Writes the classes back out to itself.
      */
     public void write() {
-       write(file, ClassWriter.COMPUTE_MAXS); 
+        write(file, ClassWriter.COMPUTE_MAXS);
     }
 
     /**
      * Clears the memory of the classes and resources.
      */
     public void clear() {
-        nodes.clear();
+        classes.clear();
         resources.clear();
     }
 }
